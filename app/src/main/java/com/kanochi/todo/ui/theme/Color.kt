d5ff22e73151d009b91a8a733e28a8a9b8d4ee1e
kanochi <kanochi@users.noreply.github.com>
2026-06-16 23:03:18 +0800

    fix: theme color deep navy (#0F172A), LowPriority separated from primary

--- app/src/main/java/com/kanochi/todo/ui/theme/Color.kt
+++ app/src/main/java/com/kanochi/todo/ui/theme/Color.kt
@@ -2,16 +2,17 @@ package com.kanochi.todo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Light theme - Blue primary, white background
+// Light theme - Dark navy primary, white background
 val AppBackground = Color(0xFFF8F9FA)
 val AppSurface = Color(0xFFFFFFFF)
 val AppSurfaceVariant = Color(0xFFF0F2F5)
 val AppBorder = Color(0xFFE5E7EB)
 
-val PrimaryBlue = Color(0xFF196090)
-val PrimaryBlueLight = Color(0xFF1E7DB8)
-val PrimaryBlueVariant = Color(0xFFD6ECF5)
-val BlueAccent = Color(0xFF145074)
+// Primary: deep navy black
+val PrimaryBlue = Color(0xFF0F172A)
+val PrimaryBlueLight = Color(0xFF1E293B)
+val PrimaryBlueVariant = Color(0xFFE2E8F0)
+val BlueAccent = Color(0xFF0B1120)
 
 val TextPrimary = Color(0xFF1F2937)
 val TextSecondary = Color(0xFF6B7280)
@@ -19,12 +20,12 @@ val TextTertiary = Color(0xFF9CA3AF)
 
 val HighPriority = Color(0xFFDC2626)
 val MediumPriority = Color(0xFFD97706)
-val LowPriority = Color(0xFF196090)
+val LowPriority = Color(0xFF0E7490)
 
 val CompletedText = Color(0xFF9CA3AF)
 val CompletedGreen = Color(0xFF16A34A)
 
-val CalendarToday = Color(0xFF2563EB)
-val CalendarTodayBg = Color(0xFFDBEAFE)
+val CalendarToday = Color(0xFF3B82F6)
+val CalendarTodayBg = Color(0xFF1E3A5F)
 val CalendarSelectedBg = Color(0xFFEFF6FF)
 val CalendarWeekend = Color(0xFF6B7280)
